a0c4f46c8af55cdd0e9c0e40111cd0301c1ce330
lab314-14@labeng.local <lab314-14@labeng.local>
2020-02-07 14:58:57 +0700

    final

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -18,3 +18,16 @@ int main()
 }
 
 //Write definition of stat() here 
+void stat(const double a[],int n,double b[]){
+    double min=a[0],max=a[0];
+    for(int i=0;i<n;i++){
+        b[0]+=a[i];
+        b[1]+=pow(a[i],2);
+        if(a[i]<=min) min=a[i];
+        if(a[i]>=max) max=a[i]; 
+    }
+    b[0]=b[0]/n;
+    b[1]=sqrt((b[1]/n)-pow(b[0],2));
+    b[2]=max;
+    b[3]=min;
+}
